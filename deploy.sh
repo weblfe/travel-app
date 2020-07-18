16b751c740ddc1b4b8ea41a12260d256825ce31f
nuokwan <994685563@qq.com>
2020-07-18 14:25:24 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+dirNow=$(cd $(dirname "$0");pwd )
+
+function stop() {
+    # shellcheck disable=SC2009
+    ps -ef | grep travel-app |grep -v grep | awk '{print $2}' | xargs kill -9
+}
+
+function update() {
+    git pull && go built
+}
+
+function start() {
+  nohup ./travel-app >app.log 2>&1 &
+}
+
+function main() {
+    cd "${dirNow}"
+    update
+    stop
+    start
+}
+
+main
