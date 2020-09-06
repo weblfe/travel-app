7667ea3228ac849458dd4e94c09a650e8d481776
nuokwan <994685563@qq.com>
2020-09-07 02:18:37 +0800

    update limit us

--- plugins/limiter.go
+++ plugins/limiter.go
@@ -16,7 +16,7 @@ const (
 		GlobalPolicyEnvKey    = "LIMIT_POLICY"
 		TokenCtxValueKey      = "token"
 		MacCtxValueKey        = "mac"
-		MinAccessTimeInterval = 10 * time.Millisecond // api 访问时间间隔
+		MinAccessTimeInterval = 100 * time.Microsecond // api 访问时间间隔
 		MaxAccessTimes        = 100                   // api 最大访问次数
 )
 
