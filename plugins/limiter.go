96f919335448bd61fac1621313025fff851695a2
nuokwan <994685563@qq.com>
2020-09-07 00:11:45 +0800

    update limit times

--- plugins/limiter.go
+++ plugins/limiter.go
@@ -17,7 +17,7 @@ const (
 		TokenCtxValueKey      = "token"
 		MacCtxValueKey        = "mac"
 		MaxAccessTimeInterval = 300 * time.Millisecond // api 访问时间间隔
-		MaxAccessTimes        = 100                     // api 最大访问次数
+		MaxAccessTimes        = 150                     // api 最大访问次数
 )
 
 type LimitResult struct {
