aceb20b6220982b7bc0371f465adae53f4387145
rustlua <wordserver2019@sina.com>
2020-11-19 13:59:08 +0800

    update user travel

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 author=weblinuxgame
-app-name=travel
+appName=travel
 version=v3.16
 
-docker build -t ${author}/travel:${version} . && docker push ${author}/travel:${version}
+docker build -t ${author}/${appName}:${version} . && docker push ${author}/${appName}:${version}
