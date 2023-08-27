26cf1dd9b8710acf9d73d2a0a2d6c8ff309c6819
nuokwan <994685563@qq.com>
2023-08-27 17:38:05 +0800

    update build

--- build/setup.py
+++ build/setup.py
@@ -3,13 +3,18 @@
 import getopt
 import os
 import sys
+import subprocess
 from string import Template
 
 image_version = os.getenv("API_VERSION")
 image_name = os.getenv("IMAGE_NAME")
 docker_file = os.getenv("DOCKERFILE")
+docker_bin = "docker"
 docker_deploy_bin = "docker-compose"
 default_password = os.getenv("MY_ROOT_PASSWORD")
+deploy_server_name = os.getenv("DEPLOY_SERVER_NAME") or "travel"
+deploy_server_dir = os.getenv("DEPLOY_SERVER_DIR") or "/data/dockers/images"
+deploy_worker_dir = os.getenv("DEPLOY_WORKER_DIR") or "/data/dockers/travel-app-docker"
 current_dir = os.getcwd()
 os.chdir(current_dir)
 
@@ -66,17 +71,19 @@ def build():
 
 
 def help_menu():
-    print('setup.py [command] -t <tag>  -f <dockerfile> -i <image> -v <version> \n')
+    print('setup.py [command] -t <tag>  -f <dockerfile> -i <image> -v <version> -d <image>\n')
     print(' tags : build , start, stop ,restart, rm , clean ')
     print(' [command] docker-compose\'s command ')
     print(' v,version build image tag version ')
     print(' i,image build image tag name ')
     print(' f,file  build docker image dockerfile ')
+    print(' d,deploy docker image to prod server')
 
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv[1:], "ht:v:i:f:", ["help", "tag=", "version=", "image=", "file="])
+        opts, args = getopt.getopt(argv[1:], "-h:-t:-v:-i:-f:-d:", ["help", "tag=", "version=", "image=", "file=",
+                                                                    "deploy="])
     except getopt.GetoptError:
         print("msg:" + getopt.GetoptError.msg, "code:" + getopt.GetoptError.opt)
         help_menu()
@@ -102,8 +109,13 @@ def main(argv):
             file = arg
         elif opt in ("-i", "--image"):
             name = arg
+        elif opt in ("-d", "--deploy"):
+            version = arg
+            action_opt = "deploy"
         else:
             return action("start")
+    if action_opt != "" and action_opt == "deploy":
+        return deploy(version)
     if action_opt != "" and action_opt != "image":
         action(action_opt)
         return 0
@@ -161,6 +173,73 @@ def image(dockerfile_name, version, name):
 
 def docker_cmd(cmd):
     return os.system(docker_deploy_bin + " " + cmd)
+
+
+def format_regex(value):
+    return value.replace('/', '\\/').replace('.', '\\.')
+
+
+def deploy(image_ver):
+    if image_ver == "":
+        print("镜像名缺失")
+        return 1
+    # # AAA:8.2,8.2表示镜像版本号
+    # docker save -o tar名称.tar AAA:8.2 BBB:5.6
+    versions = image_ver.split(":")
+    image_tmp = versions[1]
+    #  docker images -f 'reference=weblinuxgame/travel-app:v3.36' -q
+    result = subprocess.run([docker_bin, "images", "-f", 'reference={}'.format(image_ver), '-q'], capture_output=True)
+    if result is not None and result.stdout is not None:
+        image_id = str(result.stdout.decode('utf-8')).replace("\n", "")
+    else:
+        return 0
+    image_file = '{}.tar'.format(image_tmp)
+    remote_image_file = '{}/{}'.format(deploy_server_dir, image_file)
+    export_image_cmd = '{} save -o {} {}'.format(docker_bin, image_file, image_id)
+    upload_image_cmd = 'scp {} {}:{}'.format(image_file, deploy_server_name, deploy_server_dir)
+    image_import_cmd = 'ssh {} docker import {} {}'.format(deploy_server_name, remote_image_file, image_ver)
+    clear_remote_cmd = 'ssh {} rm -rf {}'.format(deploy_server_name, remote_image_file)
+    update_service_ver_cmd = ('ssh {} "sed -i.bck  \'s/weblinuxgame\\/travel-app:.*/{}/g\' {}/docker-compose.yml"'.
+                              format(deploy_server_name, format_regex(image_ver), deploy_worker_dir))
+    restart_service_cmd = ('ssh {} "cd {} ; docker-compose rm -f api ; docker-compose up -d"'.
+                           format(deploy_server_name, deploy_worker_dir))
+    # 导出镜像
+    print(export_image_cmd)
+    if os.system(export_image_cmd) == 1:
+        print("导出镜像失败")
+        return 1
+    else:
+        print("导出镜像成功")
+    # 上传镜像
+    print(upload_image_cmd)
+    if os.system(upload_image_cmd) == 1:
+        print("上传镜像失败")
+        return 1
+    # 镜像导入
+    print(image_import_cmd)
+    if os.system(image_import_cmd) == 1:
+        print("镜像导入失败")
+        return 1
+    # 镜像清理
+    print(clear_remote_cmd)
+    if os.system(clear_remote_cmd) == 1:
+        print("镜像清理失败")
+        return 1
+    # 更新docker-compose.yml
+    print(update_service_ver_cmd)
+    if os.system(update_service_ver_cmd) == 1:
+        print("更新docker-compose失败")
+        return 1
+    # 服务重启
+    print(restart_service_cmd)
+    if os.system(restart_service_cmd) == 1:
+        print("服务重启失败")
+        return 1
+    # 清理本地导出镜像文件
+    os.unlink(image_file)
+    print("---清理本地导出镜像文件---")
+    print("---服务部署完成---")
+    return 0
 
 
 if __name__ == '__main__':
